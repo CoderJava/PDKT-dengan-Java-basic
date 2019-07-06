99e0cba7c64d40f6c3a7047b2d4ec5257b5017a0
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-07-06 12:34:36 +0700

    Materi Tipe Data

---
+++ src/pertemuan1/TipeData.java
@@ -0,0 +1,24 @@
+package pertemuan1;
+
+class TipeData {
+    public static void main(String[] args) {
+        byte nilai1 = 120;
+        short nilai2 = 32000;
+        int nilai3 = 50000;
+        long nilai4 = 1234567890;
+        float nilai5 = 3.14f;
+        double nilai6 = 3.14123456789;
+        boolean nilai7 = true;
+        char nilai8 = 'Y';
+        String nilai9 = "Yudi Setiawan";
+        System.out.println("nilai 1: " + nilai1);
+        System.out.println("nilai 2: " + nilai2);
+        System.out.println("nilai 3: " + nilai3);
+        System.out.println("nilai 4: " + nilai4);
+        System.out.println("nilai 5: " + nilai5);
+        System.out.println("nilai 6: " + nilai6);
+        System.out.println("nilai 7: " + nilai7);
+        System.out.println("nilai 8: " + nilai8);
+        System.out.println("nilai 9: " + nilai9);
+    }
+}
