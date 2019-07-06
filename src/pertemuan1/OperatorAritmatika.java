375c73c00e9158814c7bb7bbe1f8b9f155e0d026
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2019-07-06 12:37:24 +0700

    Materi Operator Aritmatika

---
+++ src/pertemuan1/OperatorAritmatika.java
@@ -0,0 +1,20 @@
+package pertemuan1;
+
+class OperatorAritmatika {
+    public static void main(String[] args) {
+        int nilaiA = 10;
+        int nilaiB = 3;
+        int penjumlahan = nilaiA + nilaiB;
+        int pengurangan = nilaiA - nilaiB;
+        int perkalian = nilaiA * nilaiB;
+        int pembagian = nilaiA / nilaiB;
+        int sisaBagi = nilaiA % nilaiB;
+        System.out.println("nilai A: " + nilaiA);
+        System.out.println("nilai B: " + nilaiB);
+        System.out.println("hasil penjumlahan: " + penjumlahan);
+        System.out.println("hasil pengurangan: " + pengurangan);
+        System.out.println("hasil perkalian: " + perkalian);
+        System.out.println("hasil pembagian: " + pembagian);
+        System.out.println("hasil sisa bagi: " + sisaBagi);
+    }
+}
